5ab84f7cb15ef257b3c9d97d3c65f50b9e30cc2c
nrivkin <noah.rivkin@students.olin.edu>
2017-02-23 14:55:39 -0500

    Added outline for some helper functions, to help keep things organized

--- interactive_program.py
+++ interactive_program.py
@@ -10,3 +10,27 @@ Visualize data from imf.org to create intersting infographic
 import requests
 import pickle
 import os
+
+
+def get_data():
+    """
+    pulls data from web or, if it already is locally available, accesses
+    the apropriate file
+    """
+    pass
+
+
+def process_data(data):
+    """
+    takes data in the raw form and processes it into something we can
+    understand and manipulate, divided into relevent sublists or dictionaries
+    """
+    pass
+
+
+def visualize(data, category, flag = None):
+    """
+    takes the processed data and displays the part of the data that is
+    in the requested category. Additional options are available with a flag
+    """
+    pass
