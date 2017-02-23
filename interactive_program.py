79afb56d7015e7d0aa7914978f53b332d7f98caa
nrivkin <noah.rivkin@students.olin.edu>
2017-02-23 14:44:51 -0500

    Created main file, added header and import for some basic packages

---
+++ interactive_program.py
@@ -0,0 +1,12 @@
+"""
+SoftDes Mini-project 4
+Interactive programing
+
+Noah Rivkin and Gracey Wilson
+
+Visualize data from imf.org to create intersting infographic
+"""
+
+import requests
+import pickle
+import os
